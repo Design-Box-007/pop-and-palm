2c85f2ce9c99abe1d319e94fa372de0ab4d6f0e0
Akram-00 <aliakram9789@gmail.com>
2025-09-25 17:06:08 +0530

    blog as been update

--- src/components-2/Diaries/BlogCard1.jsx
+++ src/components-2/Diaries/BlogCard1.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-import { blogData } from "../data/blogsData";
-import { generateBlogUrl } from "../utils/blogURLGenerator";
 import { HashLink } from "react-router-hash-link";
+import { generateBlogUrl } from "../../utils/blogURLGenerator";
 
-const BlogCard = ({ imgSrc, title, description }) => {
+export const BlogCard = ({ imgSrc, title, description }) => {
   const blogUrl = generateBlogUrl(title); // Replace spaces with hyphens
 
   const handleBlogRedirect = () => {
@@ -16,7 +14,7 @@ const BlogCard = ({ imgSrc, title, description }) => {
       <div className="blog-card-body">
         <h5 className="blog-card-title">{title}</h5>
         <p className="blog-card-text">{description}</p>
-        <HashLink style={{ width: "100%" }} smooth to={`${blogUrl}#top`}>
+        <HashLink style={{ width: "100%" }} smooth to={`/blog/${blogUrl}#top`}>
           <button
             onClick={handleBlogRedirect}
             className="button-primary blog-redirect-btn"
@@ -28,23 +26,3 @@ const BlogCard = ({ imgSrc, title, description }) => {
     </div>
   );
 };
-
-const BlogsGrid = () => {
-  return (
-    <section className="blog-grid-container">
-      <h2 className="blog-grid-container-title">Recent Post</h2>
-      <div className="blog-grid-container-cards">
-        {[...blogData].reverse().map((blog, index) => (
-          <BlogCard
-            key={index}
-            imgSrc={blog.imgSrc}
-            title={blog.title}
-            description={blog.description}
-          />
-        ))}
-      </div>
-    </section>
-  );
-};
-
-export default BlogsGrid;
--- src/components-2/Diaries/DiariesHeader.jsx
+++ src/components-2/Diaries/DiariesHeader.jsx
@@ -2,17 +2,21 @@
 import React from "react";
 import styles from "./DiariesHeader.module.css";
 import { ArrowRightCircleIcon } from "lucide-react";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 function DiariesHeader({ title }) {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>{title}</h1>
-      <button>
-        View All
-        <span>
-          <ArrowRightCircleIcon />
-        </span>
-      </button>
+      <HashLink smooth to={"/blog"} className={styles.view}>
+        <button className={styles.view}>
+          View All
+          <span>
+            <ArrowRightCircleIcon />
+          </span>
+        </button>
+      </HashLink>
     </header>
   );
 }
